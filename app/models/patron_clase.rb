88669c840023d7eb86560dc611ed3fc4ddcffc36
DavidEspinola <davidep06@gmail.com>
2014-10-10 17:37:27 +0200

    Creación de primera versión de seeds y cambios menores

--- app/models/patron_clase.rb
+++ app/models/patron_clase.rb
@@ -2,3 +2,4 @@ class PatronClase < ActiveRecord::Base
 	belongs_to :asignatura
 	belongs_to :clase
 end
+#TODO: Revisar que la eliminación de asignaturas o clases destruya las relaciones existentes.
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+PatronClase.delete_all
+Clase.delete_all
+Asignatura.delete_all
+
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'clases'")
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'asignaturas'")
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'patron_clases'")
+
+for i in 1..100
+	Clase.create(nombre: "Clase #{i}", descripcion: "Esta es la clase #{i}.", recursos: "Url del SCORM en la red y otros recursos")
+end
+
+for i in 1..100
+	Asignatura.create(nombre: "Asignatura #{i}", descripcion: "Esta es la asignatura #{i}.")
+end
+
+for i in 1..100
+	for j in 1..5
+		PatronClase.create(asignatura: Asignatura.find(i), clase: Clase.find(rand(j*20)+1), duracion: rand(7)+1, orden: 6-j)
+	end
+end
