f7a24d2520105864fb31a2ce48bb1ad30b1d9877
DavidEspinola <davidep06@gmail.com>
2014-11-21 11:46:04 +0100

    Añadido Devise para la gestión de usuarios

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'jquery-rails'
 gem 'jquery-ui-rails'
 # Añadido kaminari para simplificar la paginacion
 gem 'kaminari'
+# Devise para la gestión de usuarios
+gem 'devise'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104075220) do
+ActiveRecord::Schema.define(version: 20141121104327) do
 
   create_table "asignaturas", force: true do |t|
     t.string   "nombre"
@@ -114,5 +114,23 @@ ActiveRecord::Schema.define(version: 20141104075220) do
     t.text     "tags"
     t.text     "keywords"
   end
+
+  create_table "usuarios", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "usuarios", ["email"], name: "index_usuarios_on_email", unique: true
+  add_index "usuarios", ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
 
 end
